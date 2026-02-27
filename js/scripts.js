c3d8ad274f6b7d4cf8f550efdc8f75c6627c315d
onlymatz <matheusmarcondes2@hotmail.com>
2026-02-27 20:49:08 -0300

    v0.4 - Ideia do menu hamburguer descartada, adição da seção de Liguagens de Programação com cards que possuem efeito de rotação, melhorar tamanho dos cards na versão responsiva

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,18 @@ const frm = document.querySelector("form");
 const mainIncio = document.querySelector("#main");
 const divInicio = document.querySelector("#home");
 const divAbout = document.querySelector("#about");
+const divHard = document.querySelector("#hard");
 const switchMode = () => {
     if(frm.mode.checked) {
         mainIncio.className = `back-main-light`;
         divInicio.className = `inicio-light`;
         divAbout.className = `about-me-light`;
+        divHard.className = `hard-skills-light`;
     } else {
         mainIncio.className = `back-main-dark`;
         divInicio.className = `inicio`;
         divAbout.className = `about-me-dark`;
+        divHard.className = `hard-skills`;
     }
 }
 
